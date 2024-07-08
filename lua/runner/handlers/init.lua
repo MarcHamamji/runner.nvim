9ae6f56b73471174c6c4d47581007c6781fb6b6e
Marc Hamamji <hamamjimarc@gmail.com>
2024-07-08 10:24:31 +0300

    Add jsx and tsx filetypes for nodejs handler

--- lua/runner/handlers/init.lua
+++ lua/runner/handlers/init.lua
@@ -6,6 +6,8 @@ local handlers = {
   lua = helpers.command_handler('luafile %'),
   javascript = require('runner.handlers.languages.nodejs'),
   typescript = require('runner.handlers.languages.nodejs'),
+  javascriptreact = require('runner.handlers.languages.nodejs'),
+  typescriptreact = require('runner.handlers.languages.nodejs'),
   vue = require('runner.handlers.languages.nodejs'),
   go = require('runner.handlers.languages.go'),
 }
