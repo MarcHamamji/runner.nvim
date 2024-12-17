eb089f54a675d7cf5e5b3c4fd6700971e61a095c
Marc Hamamji <hamamjimarc@gmail.com>
2024-12-17 16:15:57 +0200

    Format java handler file

--- lua/runner/handlers/languages/java.lua
+++ lua/runner/handlers/languages/java.lua
@@ -1,12 +1,12 @@
 local helpers = require('runner.handlers.helpers')
 
 return function(buffer)
-    local current_file = vim.fn.expand('%')
+  local current_file = vim.fn.expand('%')
 
-    local handlers = {
-        ['Run current file'] = helpers.shell_handler('java ' .. current_file),
-        ['Custom'] = helpers.shell_handler('java ' .. current_file, true),
-    }
+  local handlers = {
+    ['Run current file'] = helpers.shell_handler('java ' .. current_file),
+    ['Custom'] = helpers.shell_handler('java ' .. current_file, true),
+  }
 
-    helpers.choice(handlers)(buffer)
+  helpers.choice(handlers)(buffer)
 end
